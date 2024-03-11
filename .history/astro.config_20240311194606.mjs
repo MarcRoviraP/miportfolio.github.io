31a8ee4160897a3b035776e448ad79d11fd255c5
marcrovira <marcrovira@gmail.com>
2024-03-11 19:54:23 +0100

    yaml

--- .history/astro.config_20240311194606.mjs
+++ .history/astro.config_20240311194606.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://github.com/miportfolio.github.io',
+  site: 'https://https://marcrovirap.github.io/miportfolio/',
 });
